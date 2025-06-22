b41232b9a48df2bdef26973e327fa795561c5094
Luizrattacaso <rattacasoluiz03@gmail.com>
2025-06-22 18:31:51 -0300

    Iniciando testes na plataforma VsCode

---
+++ entities/PersonalInfo.java
@@ -0,0 +1,37 @@
+public class PersonalInfo {
+    private String name;
+    private int currencyGlucose;
+    private String time;
+    private int age;
+    private int insulinUnit;
+
+    public PersonalInfo(String name, int age, int insulinUnit){
+        this.name = name;
+        this.age = age;
+        this.insulinUnit = insulinUnit;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setInsulinUnit(int insulinUnit) {
+        this.insulinUnit = insulinUnit;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getInsulinUnit(){
+        return insulinUnit;
+    }
+}
